12e41ceb8a3a4b3b7941098bdbd1c4c4fb19ceac
Julius de Bruijn <bruijn@prisma.io>
2019-06-27 14:07:05 +0200

    Some async/await candy

--- src/server.rs
+++ src/server.rs
@@ -1,17 +1,31 @@
 use super::*;
 use futures01::Future;
 use futures03::future::{FutureExt, TryFutureExt};
-use std::sync::Arc;
+use std::{sync::Arc};
 
 pub struct Server<T>
 where
-    T: AsyncConnector + 'static,
+    T: AsyncConnector + Send + Sync + 'static,
 {
     db: Arc<T>,
 }
 
+/// Showing off with async/await just because...
+async fn get_four<T>(db: Arc<T>) -> Res<i64>
+where
+    T: AsyncConnector + Send + Sync + 'static
+{
+    let two = db.async_tx(|tx| {
+        let two = *tx.filter("SELECT 2").unwrap().first().unwrap();
+        Ok(two)
+    }).await?;
+
+    Ok(two * 2)
+}
+
+
 impl_web! {
-    impl<T> Server<T> where T: AsyncConnector + 'static {
+    impl<T> Server<T> where T: AsyncConnector + Send + Sync + 'static {
         pub fn new() -> Self {
             Self {
                 db: Arc::new(T::new())
@@ -20,10 +34,7 @@ impl_web! {
 
         #[get("/")]
         fn index(&self) -> impl Future<Item = String, Error = Error> + Send {
-            self.db.async_tx(|tx| {
-                let one = *tx.filter("SELECT 2").unwrap().first().unwrap();
-                Ok(one.to_string())
-            }).boxed().compat()
+            get_four(self.db.clone()).boxed().compat().map(|i| i.to_string())
         }
     }
 }
